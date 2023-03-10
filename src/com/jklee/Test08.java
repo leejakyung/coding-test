7210dafa13757db837ae035374cd592ce5ac7a0e
jakyunglee <jakyunglee@DESKTOP-RQGQG0L>
2023-03-10 09:50:32 +0900

    깃 머지 테스트

--- src/com/jklee/Test08.java
+++ src/com/jklee/Test08.java
@@ -3,19 +3,38 @@ package com.jklee;
 // 어떤 자연수를 제곱했을 때 나오는 정수를 제곱수라고 합니다. 
 // 정수 n이 매개변수로 주어질 때, n이 제곱수라면 1을 아니라면 2를 return하도록 solution 함수를 완성해주세요.
 public class Test08 {
-	
+
 	// 제곱수 판별하기 
+	//	public static void main(String[] args) {
+	//		int answer = 0;
+	//		int n = 144;
+	//		
+	//		for (int i = 1; i < n/2; i++) {
+	//			if(i*i==n) {
+	//				answer = 1;
+	//			}
+	//		}
+	//		answer = 2;
+	//		
+	//		System.out.println(answer);
+	//	}
+
+
 	public static void main(String[] args) {
-		int answer = 0;
-		int n = 144;
-		
-		for (int i = 1; i < n/2; i++) {
-			if(i*i==n) {
-				answer = 1;
-			}
-		}
-		answer = 2;
-		
-		System.out.println(answer);
+
+		Test10 t = new Test10();
+		t.add("fasd");
+		t.add("fasd");
+		t.add("fasd");
+
+
+
+		String str = "safasdasd";
+		boolean b = t.validationString(str);
+
+
+		if(t.validationString(str))
+			System.out.println(b);
+
 	}
 }
--- src/com/jklee/Test10.java
+++ src/com/jklee/Test10.java
@@ -1,5 +1,28 @@
 package com.jklee;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Test10 {
 
+	private List<String> strList = new ArrayList<>();
+	
+
+	
+	public boolean validationString(String value) {
+		
+		for(String s : strList) {
+			if(s.equalsIgnoreCase(value))
+				return true;
+		}
+		
+		return false;
+	}
+	
+	
+    public void add(String value) {
+    	strList.add(value);
+    }
+	
+	
 }
