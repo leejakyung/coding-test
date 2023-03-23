e1d0722ed992831f80a32dd6a0a42ea97b77aa7c
user <user@LAPTOP-618JVK7J>
2023-03-23 16:18:40 +0900

    2023/03/23 종이 자르기

---
+++ src/com/jklee/Test11.java
@@ -0,0 +1,13 @@
+package com.jklee;
+
+// 머쓱이는 큰 종이를 1 x 1 크기로 자르려고 합니다. 
+// 예를 들어 2 x 2 크기의 종이를 1 x 1 크기로 자르려면 최소 가위질 세 번이 필요합니다.
+
+public class Test11 {
+	public static void main(String[] args) {
+		int M = 2;
+		int N = 2;
+		
+		System.out.println(M*N-1);
+	}
+}
